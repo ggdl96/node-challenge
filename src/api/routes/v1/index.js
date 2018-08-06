a8fbc8b5d4ffa5b11d5c39ec8217651fbca59911
Gustavo Gabriel De Luca <gustavog@avalith.net>
2018-08-06 15:44:53 -0300

    Register routes

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+const searchRoute = require('./search.route');
+const viewRoute = require('./view.route');
+
 /**
  * GET v1/status
  */
 router.get('/status', (req, res) => res.send('OK'));
+
+router.use('/search', searchRoute);
 
 /**
  * GET v1/docs
  */
 router.use('/docs', express.static('docs'));
 
+router.use('/views', viewRoute);
 module.exports = router;
